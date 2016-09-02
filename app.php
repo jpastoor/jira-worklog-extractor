23368040cf8f3ba77f8250ee255e2aed9f03d943
Joost Pastoor <joost.pastoor@munisense.com>
2016-09-02 12:03:41 +0000

    Added extra command

--- app.php
+++ app.php
@@ -6,10 +6,12 @@ require __DIR__ . "/vendor/autoload.php";
 use Jpastoor\JiraWorklogExtractor\Command\ClearCacheCommand;
 use Jpastoor\JiraWorklogExtractor\Command\LoadProjectTotalsInTimeperiodCommand;
 use Jpastoor\JiraWorklogExtractor\Command\WorkedHoursPerDayCommand;
+use Jpastoor\JiraWorklogExtractor\Command\WorkedHoursPerDayPerAuthorCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 $application->add(new LoadProjectTotalsInTimeperiodCommand());
 $application->add(new WorkedHoursPerDayCommand());
+$application->add(new WorkedHoursPerDayPerAuthorCommand());
 $application->add(new ClearCacheCommand());
 $application->run();
